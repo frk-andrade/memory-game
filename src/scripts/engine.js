fcf8449ffec28ecfa805957bd804b004e17df755
frk-andrade <frkdj25@gmail.com>
2024-11-08 16:47:42 -0300

    criando os cards com itens dinamicamente

---
+++ src/scripts/engine.js
@@ -0,0 +1,28 @@
+const emojis = [
+    "😺",
+    "😺",
+    "🐲",
+    "🐲",
+    "🦋",
+    "🦋",
+    "🦇",
+    "🦇",
+    "🪅",
+    "🪅",
+    "🦝",
+    "🦝",
+    "🦄",
+    "🦄",
+    "🐵",
+    "🐵"
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for( let i=0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
